5e66f705f9a00bdf4206cf5b590cc13a0abc578c
Apple <santos.delaft@gmail.com>
2022-05-03 08:45:37 -0500

    Rubocop added and devise views generated

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# Class extension from devise
 class User::RegistrationsController < Devise::RegistrationsController
-   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -42,7 +43,8 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :encrypted_password, :password, :username])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name last_name email encrypted_password password username])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
